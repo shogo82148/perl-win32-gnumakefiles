c2f68dfb5be0795d1925145acbe3b448d6681761
Ichinose Shogo <shogo82148@gmail.com>
2020-11-16 02:31:01 +0900

    add v5.11.5

--- generate.sh
+++ generate.sh
@@ -250,6 +250,11 @@ sub _patch_gnumakefile_510 {
     _write_gnumakefile(\$version, <<'MAKEFILE');
 $(cat v5.10.0/GNUmakefile)
 MAKEFILE
+    if (_ge(\$version, "5.11.5")) {
+        _patch_gnumakefile(\$version, <<'PATCH');
+$(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.10.0/GNUmakefile v5.11.5/GNUmakefile)
+PATCH
+    }
 }
 
 sub _patch_gnumakefile_509 {
