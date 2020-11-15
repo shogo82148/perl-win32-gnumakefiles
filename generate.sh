9699a9d93f31494c211937d8adf07280428b8780
Ichinose Shogo <shogo82148@gmail.com>
2020-11-15 09:57:05 +0900

    use _patch_gnumakefile instead of _patch

--- generate.sh
+++ generate.sh
@@ -7,22 +7,22 @@ sub _patch_gnumakefile_512 {
 $(cat v5.12.0/GNUMakefile)
 MAKEFILE
     if (_ge(\$version, "5.13.8")) {
-        _patch(<<'PATCH');
+        _patch_gnumakefile(<<'PATCH');
 $(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.12.0/GNUMakefile v5.13.8/GNUMakefile)
 PATCH
     }
     if (_ge(\$version, "5.13.9")) {
-        _patch(<<'PATCH');
+        _patch_gnumakefile(<<'PATCH');
 $(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.13.8/GNUMakefile v5.13.9/GNUMakefile)
 PATCH
     }
     if (_ge(\$version, "5.13.10")) {
-        _patch(<<'PATCH');
+        _patch_gnumakefile(<<'PATCH');
 $(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.13.9/GNUMakefile v5.13.10/GNUMakefile)
 PATCH
     }
     if (_ge(\$version, "5.13.11")) {
-        _patch(<<'PATCH');
+        _patch_gnumakefile(<<'PATCH');
 $(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.13.10/GNUMakefile v5.13.11/GNUMakefile)
 PATCH
     }
@@ -48,17 +48,17 @@ sub _patch_gnumakefile_508 {
 $(cat v5.8.0/GNUMakefile)
 MAKEFILE
     if (_ge(\$version, "5.8.6")) {
-        _patch(<<'PATCH');
+        _patch_gnumakefile(<<'PATCH');
 $(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.8.0/GNUMakefile v5.8.6/GNUMakefile)
 PATCH
     }
     if (_ge(\$version, "5.8.7")) {
-        _patch(<<'PATCH');
+        _patch_gnumakefile(<<'PATCH');
 $(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.8.6/GNUMakefile v5.8.7/GNUMakefile)
 PATCH
     }
     if (_ge(\$version, "5.8.9")) {
-        _patch(<<'PATCH');
+        _patch_gnumakefile(<<'PATCH');
 $(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.8.7/GNUMakefile v5.8.9/GNUMakefile)
 PATCH
     }
