4df21a4f77ee7302a157442a9d8123bc9652eacd
Ichinose Shogo <shogo82148@gmail.com>
2020-11-21 00:09:54 +0900

    add v5.9.3

--- generate.sh
+++ generate.sh
@@ -277,9 +277,14 @@ sub _patch_gnumakefile_509 {
     _write_gnumakefile(\$version, <<'MAKEFILE');
 $(cat v5.9.0/GNUmakefile)
 MAKEFILE
+    if (_ge(\$version, "5.9.3")) {
+        _patch_gnumakefile(\$version, <<'PATCH');
+$(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.9.0/GNUmakefile v5.9.3/GNUmakefile)
+PATCH
+    }
     if (_ge(\$version, "5.9.4")) {
         _patch_gnumakefile(\$version, <<'PATCH');
-$(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.9.0/GNUmakefile v5.9.4/GNUmakefile)
+$(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.9.3/GNUmakefile v5.9.4/GNUmakefile)
 PATCH
     }
     if (_ge(\$version, "5.9.5")) {
