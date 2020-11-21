2ea3c036c41b3f7734596d6aafdd804a56cab949
Ichinose Shogo <shogo82148@gmail.com>
2020-11-21 14:26:47 +0900

    fix v5.8.3

--- generate.sh
+++ generate.sh
@@ -304,9 +304,19 @@ sub _patch_gnumakefile_508 {
     _write_gnumakefile(\$version, <<'MAKEFILE');
 $(cat v5.8.0/GNUmakefile)
 MAKEFILE
+    if (_ge(\$version, "5.8.1")) {
+        _patch_gnumakefile(\$version, <<'PATCH');
+$(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.8.0/GNUmakefile v5.8.1/GNUmakefile)
+PATCH
+    }
+    if (_ge(\$version, "5.8.2")) {
+        _patch_gnumakefile(\$version, <<'PATCH');
+$(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.8.1/GNUmakefile v5.8.2/GNUmakefile)
+PATCH
+    }
     if (_ge(\$version, "5.8.3")) {
         _patch_gnumakefile(\$version, <<'PATCH');
-$(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.8.0/GNUmakefile v5.8.3/GNUmakefile)
+$(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.8.2/GNUmakefile v5.8.3/GNUmakefile)
 PATCH
     }
     if (_ge(\$version, "5.8.5")) {
