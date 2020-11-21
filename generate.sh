627a85b7479a1db80e613b024c5c5a30c599abdc
Ichinose Shogo <shogo82148@gmail.com>
2020-11-21 19:40:48 +0900

    fix v5.7.3

--- generate.sh
+++ generate.sh
@@ -346,6 +346,11 @@ sub _patch_gnumakefile_507 {
     _write_gnumakefile(\$version, <<'MAKEFILE');
 $(cat v5.7.0/GNUmakefile)
 MAKEFILE
+    if (_ge(\$version, "5.7.3")) {
+        _patch_gnumakefile(\$version, <<'PATCH');
+$(diff -L win32/GNUmakefile -L win32/GNUmakefile -u v5.7.0/GNUmakefile v5.7.3/GNUmakefile)
+PATCH
+    }
 }
 
 sub _patch_gnumakefile_506 {
